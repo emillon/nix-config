28dc702e21cbfd760614f83ae2c5b89231532f5d
Etienne Millon <me@emillon.org>
2023-03-06 08:43:55 +0100

    sw

--- flake.nix
+++ flake.nix
@@ -67,6 +67,7 @@
                 let g:syntastic_ocaml_checkers = ['merlin']
                 let g:syntastic_always_populate_loc_list = 1
                 let g:syntastic_check_on_open = 1
+                let sw=2
               '';
             };
 
