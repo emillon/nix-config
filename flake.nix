bebaae558ea752a977a2a260632e4259873e8ecb
Etienne Millon <me@emillon.org>
2023-05-11 16:04:26 +0200

    reformat

--- flake.nix
+++ flake.nix
@@ -11,8 +11,7 @@
     let
       system = "x86_64-linux";
       username = "etienne";
-      pkgs = import nixpkgs
-      {
+      pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
@@ -23,117 +22,89 @@
         programs.home-manager.enable = true;
       };
       dev = {
-        home.packages =
-          with pkgs;
-          [
-            gh
-            opam
-            nixfmt
+        home.packages = with pkgs; [ gh opam nixfmt ];
+
+        programs.neovim = {
+          enable = true;
+          vimAlias = true;
+          viAlias = true;
+          plugins = with pkgs.vimPlugins; [
+            fzf-vim
+            molokai
+            vim-nix
+            fugitive
+            rhubarb
+            neoformat
+            nvim-lspconfig
+            nerdcommenter
+            syntastic
           ];
+          extraConfig = ''
+            let mapleader=","
+            let maplocalleader=","
+            nnoremap <silent> <C-p> :GFiles<CR>
+            nmap <LocalLeader>f :Neoformat<cr>
+            colorscheme molokai
+            inoremap jk <esc>
+            map <leader>ew :e <C-R>=expand("%:p:h") . "/" <CR>
+            map <leader>es :sp <C-R>=expand("%:p:h") . "/" <CR>
+            map <leader>ev :vsp <C-R>=expand("%:p:h") . "/" <CR>
+            map <leader>et :tabe <C-R>=expand("%:p:h") . "/" <CR>
+            let g:merlindir = trim(system('opam var share')) . "/merlin/vim"
+            if isdirectory(g:merlindir)
+            execute "set rtp+=" . g:merlindir
+            execute "helptags " . g:merlindir . "/doc"
+            endif
+            noremap <silent> <C-p> :GFiles<CR>
+            let g:syntastic_ocaml_checkers = ['merlin']
+            let g:syntastic_always_populate_loc_list = 1
+            let g:syntastic_check_on_open = 1
+            let sw=2
+          '';
+        };
 
-          programs.neovim = {
-            enable = true;
-            vimAlias = true;
-            viAlias = true;
-            plugins =
-              with pkgs.vimPlugins;
-              [
-                fzf-vim
-                molokai
-                vim-nix
-                fugitive
-                rhubarb
-                neoformat
-                nvim-lspconfig
-                nerdcommenter
-                syntastic
-              ];
-              extraConfig = ''
-                let mapleader=","
-                let maplocalleader=","
-                nnoremap <silent> <C-p> :GFiles<CR>
-                nmap <LocalLeader>f :Neoformat<cr>
-                colorscheme molokai
-                inoremap jk <esc>
-                map <leader>ew :e <C-R>=expand("%:p:h") . "/" <CR>
-                map <leader>es :sp <C-R>=expand("%:p:h") . "/" <CR>
-                map <leader>ev :vsp <C-R>=expand("%:p:h") . "/" <CR>
-                map <leader>et :tabe <C-R>=expand("%:p:h") . "/" <CR>
-                let g:merlindir = trim(system('opam var share')) . "/merlin/vim"
-                if isdirectory(g:merlindir)
-                execute "set rtp+=" . g:merlindir
-                execute "helptags " . g:merlindir . "/doc"
-                endif
-                noremap <silent> <C-p> :GFiles<CR>
-                let g:syntastic_ocaml_checkers = ['merlin']
-                let g:syntastic_always_populate_loc_list = 1
-                let g:syntastic_check_on_open = 1
-                let sw=2
-              '';
+        programs.git = {
+          enable = true;
+          userEmail = "me@emillon.org";
+          userName = "Etienne Millon";
+          extraConfig = {
+            url = {
+              "git@github.com".pushInsteadOf = "https://github.com/";
+              "git@gist.github.com".pushInsteadOf = "https://gist.github.com/";
             };
-
-            programs.git = {
-              enable = true;
-              userEmail = "me@emillon.org";
-              userName = "Etienne Millon";
-              extraConfig = {
-                url = {
-                  "git@github.com".pushInsteadOf = "https://github.com/";
-                  "git@gist.github.com".pushInsteadOf = "https://gist.github.com/";
-                };
-                merge.conflictstyle = "diff3";
-                commit.verbose = true;
-                pull.ff = "only";
-                rerere.enabled = true;
-                interactive.singlekey = true;
-                init.defaultBranch = "main";
-              };
-            };
-
-            programs.tmux.enable = true;
+            merge.conflictstyle = "diff3";
+            commit.verbose = true;
+            pull.ff = "only";
+            rerere.enabled = true;
+            interactive.singlekey = true;
+            init.defaultBranch = "main";
           };
-          x11 = {
-            home.packages =
-              with pkgs;
-              [
-                google-chrome
-                dmenu
-              ];
-              home.keyboard.options = [ "compose:rctrl" ];
-              home.sessionVariables.BROWSER = "google-chrome-stable";
-              xsession.enable = true;
-              xsession.windowManager.xmonad = {
-                enable = true;
-                enableContribAndExtras = true;
-              };
-            };
-          media = {
-            home.packages =
-              with pkgs;
-              [
-                yt-dlp
-              ];
-          };
+        };
+
+        programs.tmux.enable = true;
+      };
+      x11 = {
+        home.packages = with pkgs; [ google-chrome dmenu ];
+        home.keyboard.options = [ "compose:rctrl" ];
+        home.sessionVariables.BROWSER = "google-chrome-stable";
+        xsession.enable = true;
+        xsession.windowManager.xmonad = {
+          enable = true;
+          enableContribAndExtras = true;
+        };
+      };
+      media = { home.packages = with pkgs; [ yt-dlp ]; };
     in {
       homeConfigurations = {
         "${username}@delpech" = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
 
-          modules = [
-            common
-            dev
-            media
-            x11
-            ./delpech.nix
-          ];
+          modules = [ common dev media x11 ./delpech.nix ];
         };
         ${username} = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
 
-          modules = [
-            common
-            dev
-          ];
+          modules = [ common dev ];
         };
       };
     };
