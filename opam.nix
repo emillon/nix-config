7865f3dec8312083bff83b10df54d89a02a2b8df
Etienne Millon <me@emillon.org>
2024-01-19 11:48:24 +0100

    opam beta

--- opam.nix
+++ opam.nix
@@ -1,10 +1,10 @@
 { pkgs }:
 pkgs.opam.overrideAttrs (old: {
-  version = "2.2.0~alpha3";
+  version = "2.2.0~beta1";
   src = pkgs.fetchurl {
     url =
-      "https://github.com/ocaml/opam/releases/download/2.2.0-alpha3/opam-full-2.2.0-alpha3.tar.gz";
-    sha256 = "uvFgLRV7+QrMpPFcTMOMGKTu4IBQ+0vX8IfiVgpL1rI=";
+      "https://github.com/ocaml/opam/releases/download/2.2.0-beta1/opam-full-2.2.0-beta1.tar.gz";
+    sha256 = "wEMV2bSxXalNkZWGkENLwba1qTjWw0fhK7LCaCDdvOk=";
   };
   patches = [ ];
   configureFlags = [ "--with-vendored-deps" ];
