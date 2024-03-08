06d3e0131fba85d5f8b4e87d9694ad6a94952804
Etienne Millon <me@emillon.org>
2024-03-08 15:55:21 +0100

    eachdefaultsystem

--- flake.nix
+++ flake.nix
@@ -5,40 +5,43 @@
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      username = "etienne";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = [ (import ./overlay.nix) ];
-      };
-      common = {
-        home.stateVersion = "22.11";
-        home.username = "etienne";
-        home.homeDirectory = "/home/etienne";
-        programs.home-manager.enable = true;
-      };
-      dev = import ./dev.nix { inherit pkgs; };
-      x11 = import ./x11.nix { inherit pkgs; };
-      wayland = import ./wayland.nix { inherit pkgs; };
-      media = { home.packages = with pkgs; [ yt-dlp ]; };
-      work = { home.packages = [ pkgs.ocamlPackages.okra.okra-bin ]; };
-    in {
-      homeConfigurations = {
-        "${username}@delpech" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
-          modules = [ common dev media x11 wayland ./delpech.nix work ];
+  outputs = { nixpkgs, home-manager, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        username = "etienne";
+        common = {
+          home.stateVersion = "22.11";
+          home.username = "etienne";
+          home.homeDirectory = "/home/etienne";
+          programs.home-manager.enable = true;
         };
-        ${username} = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
-          modules = [ common dev ];
+        dev = import ./dev.nix { inherit pkgs; };
+        x11 = import ./x11.nix { inherit pkgs; };
+        wayland = import ./wayland.nix { inherit pkgs; };
+        media = { home.packages = with pkgs; [ yt-dlp ]; };
+        work = { home.packages = [ pkgs.ocamlPackages.okra.okra-bin ]; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ (import ./overlay.nix) ];
         };
-      };
-    };
+      in {
+        packages = {
+          homeConfigurations = {
+            "${username}@delpech" = home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+
+              modules = [ common dev media x11 wayland ./delpech.nix work ];
+            };
+            ${username} = home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+
+              modules = [ common dev ];
+            };
+          };
+        };
+      });
 }
