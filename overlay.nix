2a3360edd8dfede9a35851e7eb586484090b0bfa
Etienne Millon <me@emillon.org>
2024-03-11 17:00:30 +0100

    move to ocaml

--- overlay.nix
+++ overlay.nix
@@ -2,10 +2,10 @@ final: prev:
 let pkgs = prev;
 in {
   ocamlPackages = prev.ocamlPackages // {
-    get-activity-lib = import ./get-activity.nix { inherit pkgs; };
-    gitlab = import ./gitlab.nix { inherit pkgs; };
-    omd = import ./omd.nix { inherit pkgs; };
-    okra = import ./okra.nix { pkgs = final.pkgs; };
+    get-activity-lib = import ./ocaml/get-activity.nix { inherit pkgs; };
+    gitlab = import ./ocaml/gitlab.nix { inherit pkgs; };
+    omd = import ./ocaml/omd.nix { inherit pkgs; };
+    okra = import ./ocaml/okra.nix { pkgs = final.pkgs; };
   };
-  opam = import ./opam.nix { inherit pkgs; };
+  opam = import ./ocaml/opam.nix { inherit pkgs; };
 }
