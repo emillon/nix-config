e14ea034eb940485bf47c07e5e62e4cba4584fb5
Etienne Millon <me@emillon.org>
2024-03-29 18:10:32 +0100

    Add wrapper for nixgl

---
+++ nixglize.nix
@@ -0,0 +1,14 @@
+final: prev:
+let
+  pkgs = prev;
+  nixGL = pkgs.nixgl.nixGLIntel;
+  nixglize = pkg:
+    let
+      wrapped = pkgs.writeShellScriptBin pkg.pname ''
+        exec ${nixGL}/bin/nixGLIntel ${pkgs.lib.getExe pkg} "$@"
+      '';
+    in pkgs.symlinkJoin {
+      name = pkg;
+      paths = [ wrapped pkg ];
+    };
+in { alacritty = nixglize pkgs.alacritty; }
