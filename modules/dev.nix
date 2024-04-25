c2a6ba17ff89609e8cbb8901d164d1e554da078f
Etienne Millon <me@emillon.org>
2024-04-25 09:21:49 +0200

    use merlin before lsp

--- modules/dev.nix
+++ modules/dev.nix
@@ -44,15 +44,17 @@
       nnoremap <silent> <Space> :nohlsearch<Bar>:echo<CR>
     '';
     extraLuaConfig = ''
-      if vim.fn.executable('ocamllsp') == 1 then
-        require'lspconfig'.ocamllsp.setup{}
+      if vim.fn.isdirectory(vim.g.merlindir) == 0 then
+        if vim.fn.executable('ocamllsp') == 1 then
+          require'lspconfig'.ocamllsp.setup{}
+        end
+        vim.api.nvim_create_autocmd('LspAttach', {
+          group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+          callback = function(ev)
+            vim.keymap.set('n', '<leader>t', vim.lsp.buf.hover, { buffer = ev.buf })
+          end,
+        })
       end
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(ev)
-          vim.keymap.set('n', '<leader>t', vim.lsp.buf.hover, { buffer = ev.buf })
-        end,
-      })
     '';
   };
 
