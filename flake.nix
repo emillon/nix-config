620062beac6bdea7647bde25b4aa134f6d8c3120
Etienne Millon <me@emillon.org>
2024-07-29 16:54:18 +0200

    follow ts grammars as flakes

--- flake.nix
+++ flake.nix
@@ -18,66 +18,71 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
+    tree-sitter-cram-src = {
+      url = "github:tjdevries/tree-sitter-cram";
+      flake = false;
+    };
+    tree-sitter-dune-src = {
+      url = "github:emillon/tree-sitter-dune";
+      flake = false;
+    };
   };
 
-  outputs = { self, flake-utils, home-manager, nixgl, nixpkgs, ocaml }:
+  outputs =
+    { self
+    , flake-utils
+    , home-manager
+    , nixgl
+    , nixpkgs
+    , ocaml
+    , tree-sitter-cram-src
+    , tree-sitter-dune-src
+    }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [
-            ocaml.overlays.default
-            nixgl.overlay
-            (import ./overlay/nixglize.nix)
-            (
-              final: prev:
-                let
-                  callPackage = prev.callPackage;
-                in
-                {
-                  tree-sitter = prev.tree-sitter.override {
-                    extraGrammars = {
-                      cram = {
-                        src = prev.fetchFromGitHub {
-                          owner = "tjdevries";
-                          repo = "tree-sitter-cram";
-                          rev = "8cb450dfc5548b1aed2be5b3cd735c123b6fa6a8";
-                          hash = "sha256-CcLbZyHNKLA7NoQ3SLWEBCh5Y/dmzcSGQjs6iCrCFVY=";
-                        };
-                      };
-                      dune = {
-                        src = prev.fetchFromGitHub {
-                          owner = "emillon";
-                          repo = "tree-sitter-dune";
-                          rev = "a601bd0e718000db8358c279534763d6fccf7bc5";
-                          hash = "sha256-d75kpn//MZ2OZw9dElUCzeldBTwU7NIpMYGPvizz0oo=";
-                        };
-                        generate = true;
-                      };
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [
+          ocaml.overlays.default
+          nixgl.overlay
+          (import ./overlay/nixglize.nix)
+          (
+            final: prev:
+              let
+                callPackage = prev.callPackage;
+              in
+              {
+                tree-sitter = prev.tree-sitter.override {
+                  extraGrammars = {
+                    cram.src = tree-sitter-cram-src;
+                    dune = {
+                      src = tree-sitter-dune-src;
+                      generate = true;
                     };
                   };
-                  ocamlPackages = prev.ocamlPackages // {
-                    get-activity-lib = callPackage ./ocaml/get-activity.nix { };
-                    gitlab = callPackage ./ocaml/gitlab.nix { };
-                    okra = callPackage ./ocaml/okra.nix { };
-                  };
-                }
-            )
-          ];
-        };
-      in
-      {
-        formatter = pkgs.nixpkgs-fmt;
-        packages = {
-          homeConfigurations =
-            (import ./home.nix) { inherit home-manager pkgs; };
-          okra = pkgs.ocamlPackages.okra.okra-bin;
-          opam = pkgs.opam;
-        };
-        devShells.okra = pkgs.mkShell {
-          nativeBuildInputs = [ self.packages.${system}.okra ];
-          shellHook = "export ${pkgs.ocamlPackages.okra.okra-vim-env}=1";
-        };
-      });
+                };
+                ocamlPackages = prev.ocamlPackages // {
+                  get-activity-lib = callPackage ./ocaml/get-activity.nix { };
+                  gitlab = callPackage ./ocaml/gitlab.nix { };
+                  okra = callPackage ./ocaml/okra.nix { };
+                };
+              }
+          )
+        ];
+      };
+    in
+    {
+      formatter = pkgs.nixpkgs-fmt;
+      packages = {
+        homeConfigurations =
+          (import ./home.nix) { inherit home-manager pkgs; };
+        okra = pkgs.ocamlPackages.okra.okra-bin;
+        opam = pkgs.opam;
+      };
+      devShells.okra = pkgs.mkShell {
+        nativeBuildInputs = [ self.packages.${system}.okra ];
+        shellHook = "export ${pkgs.ocamlPackages.okra.okra-vim-env}=1";
+      };
+    });
 }
