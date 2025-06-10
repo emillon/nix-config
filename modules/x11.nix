b7fd9b5a9a9074caaaf58e37fc30a75b9fb09c49
Etienne Millon <me@emillon.org>
2025-06-10 16:52:11 +0200

    obsidian

--- modules/x11.nix
+++ modules/x11.nix
@@ -1,5 +1,5 @@
 { pkgs }: {
-  home.packages = with pkgs; [ google-chrome dmenu alacritty ];
+  home.packages = with pkgs; [ google-chrome dmenu alacritty obsidian ];
   home.keyboard.options = [ "compose:rctrl" ];
   home.sessionVariables.BROWSER = "google-chrome-stable";
   xsession.enable = true;
