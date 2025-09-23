749d87721e628b2e62fe22d537e55d9604a0d7ee
Etienne Millon <etienne.millon@mistral.ai>
2025-09-23 18:24:33 +0200

    Ruff config

--- modules/dev/nvim.lua
+++ modules/dev/nvim.lua
@@ -47,3 +47,16 @@ vim.g.neoformat_mld_ocamlformat = {
 }
 
 vim.g.neoformat_enabled_mld = {'ocamlformat'}
+
+require('lspconfig').ruff.setup({})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format {async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
