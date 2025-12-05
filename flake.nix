47f6dee13b4685f96a3bd8fd2ffe6bb10af8eb35
Etienne Millon <etienne.millon@mistral.ai>
2025-12-05 11:32:24 +0100

    Update tree-sitter conf

--- flake.nix
+++ flake.nix
@@ -6,18 +6,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
-    tree-sitter-cram-src = {
-      url = "github:tjdevries/tree-sitter-cram";
-      flake = false;
-    };
-    tree-sitter-dune-src = {
-      url = "github:emillon/tree-sitter-dune";
-      flake = false;
-    };
-    tree-sitter-mld-src = {
-      url = "github:emillon/tree-sitter-mld";
-      flake = false;
-    };
   };
 
   outputs =
@@ -25,38 +13,12 @@
     , flake-utils
     , home-manager
     , nixpkgs
-    , tree-sitter-cram-src
-    , tree-sitter-dune-src
-    , tree-sitter-mld-src
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
-        overlays = [
-          (
-            final: prev:
-              let
-                callPackage = prev.callPackage;
-              in
-              {
-                tree-sitter = prev.tree-sitter.override {
-                  extraGrammars = {
-                    cram.src = tree-sitter-cram-src;
-                    dune = {
-                      src = tree-sitter-dune-src;
-                      generate = true;
-                    };
-                    mld = {
-                      src = tree-sitter-mld-src;
-                      generate = true;
-                    };
-                  };
-                };
-              }
-          )
-        ];
       };
     in
     {
--- modules/ocaml/default.nix
+++ modules/ocaml/default.nix
@@ -1,17 +1,50 @@
 { pkgs, ... }:
+let
+  cramGrammar =
+    pkgs.tree-sitter.buildGrammar {
+      language = "cram";
+      version = "0.0.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "tjdevries";
+        repo = "tree-sitter-cram";
+        rev = "8cb450dfc5548b1aed2be5b3cd735c123b6fa6a8";
+        hash = "sha256-CcLbZyHNKLA7NoQ3SLWEBCh5Y/dmzcSGQjs6iCrCFVY=";
+      };
+    };
+  duneGrammar = pkgs.tree-sitter.buildGrammar {
+    language = "dune";
+    version = "0.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "emillon";
+      repo = "tree-sitter-dune";
+      rev = "c0360304e7d049e8657f40bc871deef1d37a2396";
+      hash = "sha256-mPIDX3l3/Koq7JkoOebz4B1J98qtnu/XB7nB76ZnSls=";
+    };
+    generate = true;
+  };
+  mldGrammar = pkgs.tree-sitter.buildGrammar {
+    language = "mld";
+    version = "0.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "emillon";
+      repo = "tree-sitter-mld";
+      rev = "ecfc4678394e5cfe6881e33fb410b4ae024cdbe9";
+      hash = "sha256-ugPLhg9P3dtM+7c3DzjJIR2vgJn9jN4Bo+U0HwMMnM0=";
+    };
+    generate = true;
+  };
+in
 {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
       (nvim-treesitter.withPlugins (p:
         [
-          p.cram
-          p.dune
           p.menhir
-          p.mld
           p.ocaml
           p.ocaml-interface
         ]))
-    ];
+    ] ++ map pkgs.neovimUtils.grammarToPlugin [ cramGrammar duneGrammar mldGrammar ]
+    ;
     extraConfig = builtins.readFile ./nvim.vim;
     extraLuaConfig = builtins.readFile ./nvim.lua;
   };
