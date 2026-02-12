67ee910bea7cf0b189e608f69859dcf9ac6f9a8a
Etienne Millon <me@emillon.org>
2026-02-12 15:27:40 +0100

    Use default conf for claude

--- modules/vibe.nix
+++ modules/vibe.nix
@@ -1,10 +1,5 @@
 {pkgs, ...}:
 {
   home.packages = [ pkgs.mistral-vibe ];
-
   programs.claude-code.enable = true;
-  home.sessionVariables = {
-    ANTHROPIC_MODEL = "claude-opus-4-5@20251101";
-    ANTHROPIC_DEFAULT_HAIKU_MODEL = "claude-haiku-4-5@20251001";
-  };
 }
