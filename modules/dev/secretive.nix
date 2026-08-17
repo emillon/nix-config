f134500d74ec67197e012c28b7ca1e6d7b828fd7
Etienne Millon <me@emillon.org>
2026-08-17 18:03:33 +0200

    secretive: ssh config

--- modules/dev/secretive.nix
+++ modules/dev/secretive.nix
@@ -1,9 +1,13 @@
 { config, lib, ... }:
+let
+  socket = "Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
+in
 {
   options.local.useSecretive = {
     enable = lib.mkEnableOption "Add secretive integration";
   };
-  config.home.sessionVariables = lib.mkIf config.local.useSecretive.enable {
-    SSH_AUTH_SOCK = "$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
+  config = lib.mkIf config.local.useSecretive.enable {
+    home.sessionVariables.SSH_AUTH_SOCK = "$HOME/${socket}";
+    programs.ssh.settings."*".IdentityAgent = "~/${socket}";
   };
 }
